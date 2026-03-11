ba7e93aa24dd38804d129ba418b87e102b8429d5
kimHY <83749125+guddyd622@users.noreply.github.com>
2026-03-11 14:49:01 +0900

    Add FND1.c for 7-segment display counting

---
+++ FND1.c
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+#define F_CPU 16000000
+#include <util/delay.h>
+
+unsigned char digit[10] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7c, 0x07, 0x7f, 0x67};
+unsigned char fnd_sel[4] = {0x01, 0x02, 0x04, 0x08};
+unsigned char fnd[4];
+
+int main(){
+	int i=0, count=0, j=0;
+	DDRC = 0xff;
+	DDRG = 0x0f;
+	
+	while (1) {
+		count++;
+		if (count == 10000) count = 0;
+		
+		fnd[3] = (count/1000)%10;
+		fnd[2] = (count/100)%10;
+		fnd[1] = (count/10)%10;
+		fnd[0] = count%10;
+		
+		for (j=0; j<10; j++){
+			for (i=0; i<4; i++){
+				PORTC = digit[fnd[i]];
+				PORTG = fnd_sel[i];
+				_delay_ms(2);
+				if(i%2) _delay_ms(1);
+			}
+		}
+	}
+}
